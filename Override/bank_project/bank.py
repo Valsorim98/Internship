0a7fb3f192c16b55a140ae189afc4f15ac35d8e9
Valsorim98 <vt.stanchev@gmail.com>
2021-03-14 00:21:49 +0200

    Create private function and conditional statement in main function

--- Override/bank_project/bank.py
+++ Override/bank_project/bank.py
@@ -1,6 +1,12 @@
 from date import Date
-from person import Person
 from years_later import PersonLater
+
+def __password():
+    """Create private method.
+    """    
+
+    password = str(input("Enter the password you want to sign up with: "))
+
 
 def main():
     """Create main function to run the project.
@@ -12,6 +18,9 @@ def main():
     p_age = PersonLater()    #call PersonLater() class which is sub class of Person
     p_age.introduce()        #call introduce() method
 
+    if age >= 18:
+        print(f"You can open a bank account!")
+        __password()
 
 if __name__ == "__main__":
     main()
