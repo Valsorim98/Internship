dcdc3f3adaba44fe7d9bd979da78109f6016098d
Valsorim98 <vt.stanchev@gmail.com>
2021-03-13 17:43:26 +0200

    Import years_later and call PersonLater class instead of its parent class

--- Override/bank_project/bank.py
+++ Override/bank_project/bank.py
@@ -1,5 +1,6 @@
 from date import Date
 from person import Person
+from years_later import PersonLater
 
 def main():
     """Create main function to run the project.
@@ -8,8 +9,8 @@ def main():
     date = Date()       #call Date() class
     date.today_date()   #call today_date() method
 
-    p_age = Person()    #call Person() class
-    p_age.introduce()   #call introduce() method
+    p_age = PersonLater()    #call PersonLater() class which is sub class of Person
+    p_age.introduce()        #call introduce() method
 
 
 if __name__ == "__main__":
