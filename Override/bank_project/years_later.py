0a8ba92e3621f3670ddb21dafd562f4e1aea70d4
Valsorim98 <vt.stanchev@gmail.com>
2021-03-13 17:45:38 +0200

    Create years_later.py with PersonLater class and introduce method

---
+++ Override/bank_project/years_later.py
@@ -0,0 +1,10 @@
+from person import Person
+
+class PersonLater(Person):
+
+    def introduce(self):
+        """Create method introduce.
+        """        
+
+        age = int(input("Please enter your age: "))
+        print(f"Hi, my name is Ivan Ivanov and I am {age} years old.")
