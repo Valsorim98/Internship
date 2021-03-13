b6e15b74d1d4f73cdfa770f51f5f0a3065c60443
Valsorim98 <vt.stanchev@gmail.com>
2021-03-13 17:22:55 +0200

    Create docstrings

--- Override/bank_project/date.py
+++ Override/bank_project/date.py
@@ -1,8 +1,12 @@
 from datetime import date
 
 class Date():
+    """Create class Date.
+    """    
 
     def today_date(self):
+        """Create method today_date.
+        """        
 
         today = date.today()
         d1 = today.strftime("%d/%m/%Y")     #day/month/year format
--- Override/bank_project/person.py
+++ Override/bank_project/person.py
@@ -1,7 +1,11 @@
 
 class Person():
+    """Create class Person.
+    """    
 
     def introduce(self):
+        """Create method introduce.
+        """        
 
         age = int(input("Please enter your age: "))
         print(f"Hi, my name is Ivan Ivanov and I am {age} years old.")
