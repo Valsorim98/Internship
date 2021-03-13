29ce7acc3f1225da13f8bcd9f391f3e37b4bf42a
Valsorim98 <vt.stanchev@gmail.com>
2021-03-13 17:21:37 +0200

    Create docstring, import and call Person class

--- Override/bank_project/bank.py
+++ Override/bank_project/bank.py
@@ -1,9 +1,16 @@
 from date import Date
+from person import Person
 
 def main():
+    """Create main function to run the project.
+    """    
 
-    date = Date()
-    date.today_date() 
+    date = Date()       #call Date() class
+    date.today_date()   #call today_date() method
+
+    p_age = Person()    #call Person() class
+    p_age.introduce()   #call introduce() method
+
 
 if __name__ == "__main__":
     main()
