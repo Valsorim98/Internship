4da5f8121f12be43885e96e2b564c2daf293882d
Valsorim98 <vt.stanchev@gmail.com>
2021-03-15 15:39:08 +0200

    Add two variables in while cycle and a conditional statement

--- Task1/card_list.py
+++ Task1/card_list.py
@@ -1,13 +1,22 @@
 import serial
 
-serialPort = serial.Serial(port = "COM3", baudrate = 9600,
-                            bytesize = 8, timeout = 2, stopbits = serial.STOPBITS_ONE)
+class Tokens():
 
-serialPort.open()
-serialString = ""
+    serialPort = serial.Serial(port = "COM3", baudrate=9600,
+                                bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
-while(1):
+    serialString = ""
 
-    if (serialPort.in_waiting > 0):
-        serialString = serialPort.readline()
-        print(serialString.decode('Ascii'))
+    while(1):
+
+        if (serialPort.in_waiting > 0):
+            serialString = serialPort.readline()
+            x = serialString.decode('Ascii')
+            rep = x.replace("?\r\n", "")
+            print(rep)
+
+        
+        if rep == "6E536046010080FF"
+            print("Access granted.")
+        else:
+            print("Access denied.")
