85b07bff10bd29339af05e4b9acbbcee6928ce71
Valsorim98 <vt.stanchev@gmail.com>
2021-03-15 20:40:44 +0200

    Refactor card_reader.py

--- Task1/card_reader.py
+++ Task1/card_reader.py
@@ -1,35 +1,26 @@
 import serial
+from whitelist import Whitelist
 
 class ACT_230_RFID():
 
-    def __init__(self, port, baudrate, bytesize, timeout, stopbits):
-        """Constructor for card readers.
-
-        Args:
-            port (str): Which port is the card reader using.
-            baudrate (int): The baudrate of the card reader.
-            bytesize (int): The bytesize of the card reader.
-            stopbits (int): The card reader is using one stopbit.
-        """
-
-        self.port = port
-        self.baudrate = baudrate
-        self.bytesize = bytesize
-        self.stopbits = stopbits
+    serialPort = serial.Serial(port = "COM3", baudrate=9600,
+                                bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
     def update(self):
 
-        serialString = ""
+        while(1):
 
-        serialPort = serial.Serial(port = "COM3", baudrate=9600,
-                                    bytesize=8, stopbits=serial.STOPBITS_ONE)
+            if (self.serialPort.in_waiting > 0):
+                serialString = self.serialPort.readline()
+                x = serialString.decode('Ascii')
+                rep = x.replace("?\r\n", "")
+                print(rep)
 
-        if (serialPort.in_waiting > 0):
-            serialString = serialPort.readline()
-            x = serialString.decode('Ascii')
-            rep = x.replace("?\r\n", "")
-            print(rep)
-            if rep == "6E536046010080FF":
-                print("Access granted.")
-            else:
-                print("Access denied.")
+                if rep == "6E536046010080FF":
+                    print("Access granted.")
+                else:
+                    print("Access denied.")
+
+
+           # for i in Whitelist().arr_cards:
+           #     rep == i.
