03329aa1d18e988c849f6148422107ddd646a7f4
Valsorim98 <vt.stanchev@gmail.com>
2021-03-15 16:51:19 +0200

    Refactor card_list.py

--- Task1/card_list.py
+++ Task1/card_list.py
@@ -1,23 +1,10 @@
-import serial
+from card_reader import ACT_230_RFID
 
 
 def main():
-    serialPort = serial.Serial(port = "COM3", baudrate=9600,
-                                bytesize=8, stopbits=serial.STOPBITS_ONE)
 
-    serialString = ""
+    
 
-    while(1):
-
-        if (serialPort.in_waiting > 0):
-            serialString = serialPort.readline()
-            x = serialString.decode('Ascii')
-            rep = x.replace("?\r\n", "")
-            print(rep)
-            if rep == "6E536046010080FF":
-                print("Access granted.")
-            else:
-                print("Access denied.")
 
 
 if __name__ == "__main__":
