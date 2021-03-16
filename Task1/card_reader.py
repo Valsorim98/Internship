2fe40ab8317af6b13995be2a26c38225ff8e10e2
Valsorim98 <vt.stanchev@gmail.com>
2021-03-16 13:11:25 +0200

    Remove list with com ports and fix update method

--- Task1/card_reader.py
+++ Task1/card_reader.py
@@ -1,13 +1,9 @@
 import serial
-import serial.tools.list_ports
 from whitelist import Whitelist
 
 class ACT_230_RFID():
     """Create class for card readers.
     """
-
-    comPorts = list(serial.tools.list_ports.comports())
-    print(comPorts)
 
     def __init__(self, port):
         """Constructor for card readers.
@@ -16,16 +12,15 @@ class ACT_230_RFID():
             port (str): Which port is the card reader using.
         """        
         
-        port = port
-        __serialPort = serial.Serial(port = "COM3", baudrate=9600,
+        self.__serialPort = serial.Serial(port = port, baudrate=9600,
                                     bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
     def update(self):
         """Method to get the input from a token, decode it to a string and compare to see if its whitelisted.
         """        
 
-        if (self.serialPort.in_waiting > 0):
-            serialString = self.serialPort.readline()
+        if (self.__serialPort.in_waiting > 0):
+            serialString = self.__serialPort.readline()
             x = serialString.decode('Ascii')
             rep = x.replace("?\r\n", "")
             print(rep)
