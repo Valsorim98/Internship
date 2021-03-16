107eefadf947fa241be23602bdaefca488e4b274
Valsorim98 <vt.stanchev@gmail.com>
2021-03-16 14:05:46 +0200

    Add a comment

--- Task1/whitelist.py
+++ Task1/whitelist.py
@@ -28,4 +28,5 @@ class Whitelist():
 
     arr_cards = [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18]
     #whitelist i blacklist na json file - database
+    #unix timestamp
     
