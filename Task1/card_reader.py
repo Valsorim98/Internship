ffa8703f44edeb936ad08787401ef34f8c968690
Valsorim98 <vt.stanchev@gmail.com>
2021-03-16 10:49:21 +0200

    Add docstrings and comments

--- Task1/card_reader.py
+++ Task1/card_reader.py
@@ -2,26 +2,34 @@ import serial
 from whitelist import Whitelist
 
 class ACT_230_RFID():
-
+    """Create class for card readers.
+    """
     def __init__(self, port):
-        port = port
+        """Constructor for card readers.
 
+        Args:
+            port (str): Which port is the card reader using.
+        """        
+        
+        port = port
         __serialPort = serial.Serial(port = "COM3", baudrate=9600,
                                     bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
     def update(self):
+        """Method to get the input from a token, decode it to a string and compare to see if its whitelisted.
+        """        
 
-            if (self.serialPort.in_waiting > 0):
-                serialString = self.serialPort.readline()
-                x = serialString.decode('Ascii')
-                rep = x.replace("?\r\n", "")
-                print(rep)
+        if (self.serialPort.in_waiting > 0):
+            serialString = self.serialPort.readline()
+            x = serialString.decode('Ascii')
+            rep = x.replace("?\r\n", "")
+            print(rep)
 
-                if rep == "6E536046010080FF":
-                    print("Access granted.")
-                else:
-                    print("Access denied.")
+            if rep == "6E536046010080FF":
+                print("Access granted.")
+            else:
+                print("Access denied.")
 
 
-           # for i in Whitelist().arr_cards:
-           #     rep == i.
+       # for i in Whitelist().arr_cards:
+       #     rep == i.
--- Task1/main.py
+++ Task1/main.py
@@ -2,9 +2,12 @@ from card_reader import ACT_230_RFID
 
 
 def main():
-
+    """Main function for the project.
+    """
+    #Create card reader one
     c1 = ACT_230_RFID("COM3")
 
+    #call update method in a while cycle to check for token input non-stop
     while(1):
         c1.update()
 
