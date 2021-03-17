d85885dec1d926b0d2717a4864a722ff7332ebf7
Valsorim98 <vt.stanchev@gmail.com>
2021-03-17 13:45:53 +0200

    Load database.json and remove comments

--- Task1/tokensbase.py
+++ Task1/tokensbase.py
@@ -1,15 +1,9 @@
 import json
-#from database import whitelist
+
+f = open("database.json")
+data = json.load(f)
+print(data)
 
 
-#create list of blacklist cards
-#black_cards = [bc1, bc2, bc3, bc4, bc5, bc6, bc7, bc8, bc9, bc10,
-                #bc11, bc12, bc13, bc13, bc14, bc15, bc16, bc17, bc18]
 
-#convert the list in json format
-#blacklist = json.dumps(black_cards)
-#print(blacklist)
-
-#rename to tokensbase.py
-#open database.json i podavam json.dumps
 #create access control class
