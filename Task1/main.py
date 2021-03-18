1567c91f318ece273bcf87738fb29ee518c33790
Valsorim98 <vt.stanchev@gmail.com>
2021-03-18 19:38:51 +0200

    Create a controller in main function and add comments

--- Task1/main.py
+++ Task1/main.py
@@ -1,18 +1,28 @@
-from card_reader import ACT230
-from tokensbase import Tokens
-from access_control import AccessControl
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
 
+from card_reader import ACT230
+from tokens_base import Tokens
+from io_controller import IOController
+
+from access_control import AccessControl
 
 def main():
     """Main function for the project.
     """
-    #Create card reader one
+
+    # Create card reader one
     card_reader = ACT230("COM3")
 
+    # Call class Tokens and get_database method
     tokens = Tokens()
     tbase = tokens.get_database()
 
-    ac = AccessControl(card_reader, tbase)
+    # Create controller
+    controller = IOController()
+
+    # Call AccessControl class
+    ac = AccessControl(card_reader, tbase, controller)
 
     #call update method in a while cycle to check for token input non-stop
     while(1):
