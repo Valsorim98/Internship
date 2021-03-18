da96411e057341d55f533f08faf25f464b7a151b
Valsorim98 <vt.stanchev@gmail.com>
2021-03-18 19:34:31 +0200

    Add controller in constructor, check if card id is in whitelist or blacklist database to unlock/lock the door

--- Task1/access_control.py
+++ Task1/access_control.py
@@ -1,4 +1,9 @@
-from tokensbase import Tokens
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+import time
+
+from tokens_base import Tokens
 
 class AccessControl():
     """Access control
@@ -12,7 +17,23 @@ class AccessControl():
     """Tokens database instance.
     """
  
-    def __init__(self, card_reader, tokens_base):
+    __controller = None
+    """IO Controller
+    """
+
+    __unlock_flag = False
+    """Unlock flag.
+    """
+
+    __time_to_open = 5
+    """Time to open the door in [s].
+    """
+
+    __unlocked_flag = False
+
+    __last_time = 0
+
+    def __init__(self, card_reader, tokens_base, controller):
         """Constructor
 
         Args:
@@ -24,6 +45,9 @@ class AccessControl():
 
         self.__tokens_base = tokens_base
 
+        self.__controller = controller
+
+
         if self.__card_reader is not None:
             self.__card_reader.set_card_cb(self.__card_reader_cb)
 
@@ -32,24 +56,60 @@ class AccessControl():
         if self.__tokens_base is None:
             return
 
-        print(card_id)
-        tbase = Tokens().get_database()
 
-        i = 0
-        while i < 30:
-            print(tbase)
-            i += 1
+        whitelist = self.__tokens_base["whitelist"]
 
-        #if card_id == "6E536046010080FF":
-        #    print("Access granted.")
-        #else:
-        #    print("Access denied.")
+        wl_flag = False
+        
+        for item in whitelist:
 
-        #check for code from whitelist in database
-        #if not in whitelist its in blacklist
+            db_card_id = item["code"]
+
+            if db_card_id == card_id:
+
+                wl_flag = True
+                break
+
+        
+        blacklist = self.__tokens_base["blacklist"]
+
+        bl_flag = False
+        for item in blacklist:
+            db_card_id = item["code"]
+            if db_card_id == card_id:
+
+                bl_flag = True
+                break
+
+        if wl_flag and not bl_flag:
+            print("dobrojelatel")
+            self.__unlock_flag = True
+
+        if not wl_flag and bl_flag:
+            print("nejelan")
+
+        if not wl_flag and not bl_flag:
+            print("nepoznat")
+
+        if wl_flag and bl_flag:
+            print("Sistemna greshka")
 
 
     def update(self):
 
+        now_time = time.time()
+
         if self.__card_reader is not None:
             self.__card_reader.update()
+
+        if self.__unlock_flag and not self.__unlocked_flag:
+            self.__controller.set_gpio(0, True)
+            self.__last_time = time.time()
+            self.__unlocked_flag = True
+
+        if now_time > self.__last_time + self.__time_to_open:
+            if self.__unlock_flag and self.__unlocked_flag:
+                self.__controller.set_gpio(0, False)
+                self.__unlock_flag = False
+                self.__unlocked_flag = False
+
