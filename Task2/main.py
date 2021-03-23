355883984c2eb505715656ef9ab503b6a81e1959
Valsorim98 <vt.stanchev@gmail.com>
2021-03-23 13:31:27 +0200

    Add conditional statements to see if the students get scholarships based on their average grade

--- Task2/main.py
+++ Task2/main.py
@@ -23,8 +23,28 @@ def main():
             grades.append(grade)
             grades_list.append(grade)
         av_grade = sum(grades_list) / len(grades_list)
-        print(f"{name}'s grades are: {grades_list}")
         print(f"{name}'s average grade is: {av_grade}")
+        
+        if 4.5 <= av_grade < 5.5:
+            good_scholarship = True
+        else:
+            good_scholarship = False
+
+
+        if av_grade >= 5.5:
+            excellent_scholarship = True
+        else:
+            excellent_scholarship = False
+
+        if excellent_scholarship == True:
+            print(f"{name} gets a scholarship for excellent progress!")
+
+        if good_scholarship == True:
+            print(f"{name} gets a scholarship for very good progress!")
+
+        if excellent_scholarship == False and good_scholarship == False:
+            print(f"{name} should study more to get a scholarship!")
+
 
     ug.save()
     print("Ready...")
