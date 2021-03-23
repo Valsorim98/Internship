2c239222cc252078c18811f02d94d2c6132c6690
Valsorim98 <vt.stanchev@gmail.com>
2021-03-23 12:03:57 +0200

    Add student's grades to a list and get each student's average grade

--- Task2/main.py
+++ Task2/main.py
@@ -16,10 +16,15 @@ def main():
     for student in students:
         grades = student["grade"]
         grades.clear()
+        grades_list = []
         for subject in subjects:
             name = student["name"]
             grade = int(input(f"Enter {name}'s grade for {subject}: "))
             grades.append(grade)
+            grades_list.append(grade)
+        av_grade = sum(grades_list) / len(grades_list)
+        print(f"{name}'s grades are: {grades_list}")
+        print(f"{name}'s average grade is: {av_grade}")
 
     ug.save()
     print("Ready...")
