047a983988194f1d729de79b28de233cf9080f86
Valsorim98 <vt.stanchev@gmail.com>
2021-03-23 15:19:57 +0200

    Create person consctructor in person.py

---
+++ Task2/Project2/person.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+class Person():
+    """Class Person.
+    """
+
+    def __init__(self, name, type):
+
+        self.name = name
+        self.type = type
