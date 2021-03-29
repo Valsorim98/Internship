cb2801dafba70ef141ca537bdce4a883fad7dcfc
Valsorim98 <vt.stanchev@gmail.com>
2021-03-29 17:19:54 +0300

    Add comments and a print

--- Task3/get.py
+++ Task3/get.py
@@ -5,6 +5,18 @@ payload = {"key1": "value1", "key2": "value2"}
 r = requests.get(url, params=payload)
 print(r.url)
 print(r.status_code)
+# prints the content in bytes
 print(r.content)
+# prints the content in string
 print(r.text)
+# prints the headers from which we can see the content type and other information
 print(r.headers)
+
+
+if r.status_code == 200:
+    print("Success!")
+else:
+    print("No connection.")
+
+# Convert to a dictionary
+print(r.json())
