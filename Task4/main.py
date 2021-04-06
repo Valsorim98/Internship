0afaac5a123fc957bb3b0d1b6da888b3527be549
Valsorim98 <vt.stanchev@gmail.com>
2021-04-06 13:49:58 +0300

    Turning all relays ON/OFF

--- Task4/main.py
+++ Task4/main.py
@@ -3,17 +3,45 @@
 
 import pymodbus
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+from pymodbus.constants import Defaults
+
 
 client = ModbusClient(method="rtu", port="COM4", timeout=1, stopbits=1, bytesize=8, parity="N", baudrate=9600)
-connection = client.connect()
-print(connection)
 
-while True:
+def main():
+    global client
 
-    status = client.read_coils(address=0x01, starting_address=0x0011, coil_quantity=0x0004, unit=1)
-    print(status)
+    connection = client.connect()
+    print(connection)
 
+    response = client.read_coils(
+        address=16,
+        count=4,
+        unit=1)
 
-    # turn one coil on
-    #status = client.write_coil(address=0x01, function_code=0x05, unit=1)
-    #print(status)
+    print(response.bits[:4])
+    if response.bits[:4] == [True]*4:
+        print("All relays are on.")
+    elif response.bits[:4] == [False]*4:
+        print("All relays are off.")
+    else:
+        print("Error")
+
+    # turn all coils ON
+    response = client.write_coils(16, [True]*4, unit=1)
+
+    response = client.read_coils(
+        address=16,
+        count=4,
+        unit=1)
+
+    print(response.bits[:4])
+    if response.bits[:4] == [True]*4:
+        print("All relays are turned on.")
+    elif response.bits[:4] == [False]*4:
+        print("All relays are turned off.")
+    else:
+        print("Error")
+
+if __name__ == "__main__":
+    main()
