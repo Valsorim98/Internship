6232a673f9a5ee6f011b6338d36a972f3588992d
Valsorim98 <vt.stanchev@gmail.com>
2021-04-13 16:55:01 +0300

    Create main.py for Task5 and reading the coils

---
+++ Task5/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import pymodbus
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+
+
+client = ModbusClient(method="rtu", port="COM5", timeout=1, stopbits=1, bytesize=8, parity="N", baudrate=9600)
+
+def main():
+    global client
+
+    connection = client.connect()
+
+    if connection:
+        print("Connected")
+    else:
+        print("No connection")
+
+    response = client.read_coils(
+    address=1,
+    count=8,
+    unit=1)
+
+    print(response.bits)
+
+    response = client.read_holding_registers
+
+
+
+if __name__ == "__main__":
+    main()
