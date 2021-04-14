a04c2ef80948a748efb95f94bed827e93e347b51
Valsorim98 <vt.stanchev@gmail.com>
2021-04-14 16:18:30 +0300

    Remove the connection with the controller from main.py

--- Task1/main.py
+++ Task1/main.py
@@ -1,30 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
-import pymodbus
-from pymodbus.client.sync import ModbusSerialClient as ModbusClient
-
 from card_reader import ACT230
 from tokens_base import Tokens
 from io_controller import IOController
 from access_control import AccessControl
 
 
-client = ModbusClient(method="rtu", port="COM5", timeout=1, stopbits=1, bytesize=8, parity="N", baudrate=9600)
-
 def main():
     """Main function for the project.
     """
-    global client
-
-    # Create a connection with the controller
-    connection = client.connect()
-
-    if connection:
-        print("Connected")
-    else:
-        print("No connection")
-
 
     # Create card reader one
     card_reader = ACT230("COM3")
@@ -42,8 +27,6 @@ def main():
     #call update method in a while cycle to check for token input non-stop
     while(1):
         ac.update()
-
-
 
 if __name__ == "__main__":
     main()
