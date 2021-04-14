74996bf793649256ccce966b8eb97734469445bf
Valsorim98 <vt.stanchev@gmail.com>
2021-04-14 11:26:12 +0300

    Write the coils and read them before and after writing

--- Task5/main.py
+++ Task5/main.py
@@ -8,8 +8,11 @@ from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 client = ModbusClient(method="rtu", port="COM5", timeout=1, stopbits=1, bytesize=8, parity="N", baudrate=9600)
 
 def main():
+    """Main function.
+    """    
     global client
 
+    # Create a connection with the device
     connection = client.connect()
 
     if connection:
@@ -17,13 +20,25 @@ def main():
     else:
         print("No connection")
 
+    # Read all the coils
     response = client.read_coils(
     address=1,
-    count=8,
+    count=12,
     unit=1)
 
-    print(response.bits)
+    print(response.bits[:12])
 
+    # Write all the coils ON/OFF for testing
+    # The controller has 12 coils, last four coils are turned to False to remind that the request is in 16 bits.
+    response = client.write_coils(1, [True]*12+[False]*4, unit=1)
+
+    # Read all the coils
+    response = client.read_coils(
+    address=1,
+    count=12,
+    unit=1)
+
+    print(response.bits[:12])
 
 
 if __name__ == "__main__":
