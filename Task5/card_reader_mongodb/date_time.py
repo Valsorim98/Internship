c61e0d56aa6fcbfc1e5a40b71b0ec69102713494
Valsorim98 <vt.stanchev@gmail.com>
2021-04-16 12:26:24 +0300

    Create date_time.py to get the date and time of execution of the program

---
+++ Task5/card_reader_mongodb/date_time.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+import datetime
+
+class DateTime():
+    """Class for date and time.
+    """    
+
+    def time_of_execution(self):
+        """Method for showing the execution date and time of the program.
+        """        
+
+        mydate = datetime.datetime.now()
+        print(mydate.strftime('Date of execution of the program is: %d %B %Y at %H:%M:%S'))
+
+        return mydate
+    
