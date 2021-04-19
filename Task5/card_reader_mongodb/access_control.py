b71e0065b7991133f4d0a5d5be27b4a54b515e1a
Valsorim98 <vt.stanchev@gmail.com>
2021-04-19 18:20:17 +0300

    Create instance of tokens database and insert data as a document into the database

--- Task5/card_reader_mongodb/access_control.py
+++ Task5/card_reader_mongodb/access_control.py
@@ -13,6 +13,10 @@ class AccessControl():
     """Card reader instance.
     """
 
+    __readDB = None
+    """Tokens database instance from MongoDB
+    """
+
     __tokens_base = None
     """Tokens database instance.
     """
@@ -33,7 +37,7 @@ class AccessControl():
 
     __last_time = 0
 
-    def __init__(self, card_reader, tokens_base, controller):
+    def __init__(self, card_reader, readDB, controller):
         """Constructor
 
         Args:
@@ -43,9 +47,11 @@ class AccessControl():
     
         self.__card_reader = card_reader
 
-        self.__tokens_base = tokens_base
+        self.__readDB = readDB
+        self.__tokens_base = self.__readDB.get_tokens()
 
         self.__controller = controller
+
 
 
         if self.__card_reader is not None:
@@ -53,13 +59,23 @@ class AccessControl():
 
     def __card_reader_cb(self, card_id):
 
-        if self.__tokens_base is None:
+        if self.__readDB is None:
             return
 
+        # Time of event
+        ts = int(time.time())
+
+        # State of the token for the event.
+        token_state = 0
 
         whitelist = self.__tokens_base["whitelist"]
 
         wl_flag = False
+
+        # # Print the codes from whitelist
+        # whitelist = db.tokens_database.find()
+        # for code in whitelist:
+        #     print('{0} {1}'.format(code['code']))
         
         for item in whitelist:
 
@@ -83,17 +99,29 @@ class AccessControl():
 
         if wl_flag and not bl_flag:
             print("Access granted")
+            token_state = 1
             self.__unlock_flag = True
 
         if not wl_flag and bl_flag:
             print("Access denied")
+            token_state = 2
 
         if not wl_flag and not bl_flag:
             print("Unauthorized")
+            token_state = 3
 
         if wl_flag and bl_flag:
             print("System error")
+            token_state = 4
 
+
+        entry = {"timestamp": ts,
+            "reader_id": self.__card_reader.reader_id,
+            "token_id": card_id,
+            "state": token_state,
+            "direction": 1}
+
+        self.__readDB.insert_data(entry)
 
     def update(self):
 
