215022ce6fd8929fac13ce9558ee656c4c9ec264
Valsorim98 <vt.stanchev@gmail.com>
2021-04-21 14:26:43 +0300

    Create main function for the project

---
+++ Task6/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+from card_reader import ACT230
+from tokens_base import Tokens
+from access_control import AddToken
+
+def main():
+    """Main function for the project.
+    """
+
+    # Create card reader one
+    card_reader = ACT230("COM3", 1914)
+
+    # Call class Tokens and get_tokens method
+    tokens = Tokens()
+
+
+    # Call AddToken class
+    ac = AddToken(card_reader, tokens)
+    
+
+    #call update method in a while cycle to check for token input non-stop
+    while(1):
+        ac.update()
+
+
+if __name__ == "__main__":
+    main()
