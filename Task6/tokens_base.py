972f0f3817ae54fd3abe1e0054c6dc86e07d7aec
Valsorim98 <vt.stanchev@gmail.com>
2021-04-21 12:17:27 +0300

    Create methods to read the database and insert data in MongoDB database in tokens_base.py

---
+++ Task6/tokens_base.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import pymongo
+from pymongo import MongoClient
+
+class Tokens():
+
+    def get_tokens(self):
+
+        url = "mongodb+srv://user:user-pass@cluster0.jfrs3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+        client = MongoClient(url)
+        db = client["test_db"]
+        collection = db["tokens_database"]
+
+        return collection
+
+    def insert_data(self, db_name, collection_name, data):
+
+        url = "mongodb+srv://user:user-pass@cluster0.jfrs3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+        
+        client = MongoClient(url)
+
+        db = client[db_name]
+        collection = db[collection_name]
+
+        db.entries.insert(data)
+        
