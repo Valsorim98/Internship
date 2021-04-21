a523430ed84f3b51f315a4cb3a08d7f0bfb45099
Valsorim98 <vt.stanchev@gmail.com>
2021-04-21 13:50:10 +0300

    Add import, rename class, create method update and add prints in access_control.py

--- Task6/access_control.py
+++ Task6/access_control.py
@@ -5,8 +5,9 @@ import pymongo
 from pymongo import MongoClient
 
 from tokens_base import Tokens
+from card_reader import ACT230
 
-class AccessControl():
+class AddToken():
     """Access control
     """
 
@@ -28,28 +29,28 @@ class AccessControl():
     
         self.__card_reader = card_reader
 
-        self.__readDB = readDB
-        self.__tokens_base = self.__readDB.get_tokens()
+        url = "mongodb+srv://user:user-pass@cluster0.jfrs3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+        self.__client = MongoClient(url)
 
         if self.__card_reader is not None:
             self.__card_reader.set_card_cb(self.__card_reader_cb)
+
+    def update(self):
+
+        self.__card_reader.update()
 
     def __card_reader_cb(self, card_id):
 
         if self.__readDB is None:
             return
 
-        url = "mongodb+srv://user:user-pass@cluster0.jfrs3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-        client = MongoClient(url)
-        db = client["test_db"]
-        collection = db.tokens_database
-        collection = collection.find_one({}, {"_id": 0})
+        exp_date_str = str(input("Type expiration date in day.month.year hour.minute.seconds: "))
+        print(exp_date_str)
 
-        # # Print the codes from whitelist
-        # for item in collection["whitelist"]:
-        #     print(item["code"])
+
 
         entry = {"token_id": card_id,
                 "exp_date": 1672351200}
 
-        self.__readDB.insert_data("test_db", "tokens_database", entry)
+        print(entry)
+        #self.__readDB.insert_data("test_db", "tokens_database", entry)
