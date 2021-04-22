e2f946f4194b5f2150169d385d9e2d13e6a1412b
Valsorim98 <vt.stanchev@gmail.com>
2021-04-22 16:57:06 +0300

    Create a method to update a collection with new doc in tokens_base.py

--- Task6/tokens_base.py
+++ Task6/tokens_base.py
@@ -5,8 +5,15 @@ import pymongo
 from pymongo import MongoClient
 
 class Tokens():
+    """Tokens class.
+    """    
 
     def get_tokens(self):
+        """Method to return a database collection.
+
+        Returns:
+            object: Database collection.
+        """        
 
         url = "mongodb+srv://user:user-pass@cluster0.jfrs3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
         client = MongoClient(url)
@@ -15,15 +22,18 @@ class Tokens():
 
         return collection
 
-    def insert_data(self, db_name, collection_name, data):
+    def insert_data(self, db_name, collection_name, card_id, data):
+        """Method to update a collection with new documents.
+
+        Args:
+            db_name (string): Name of the database.
+            collection_name (string): Name of the collection.
+            card_id (string): Token code.
+            data (dict): Data to be updated to the collection.
+        """        
 
         url = "mongodb+srv://user:user-pass@cluster0.jfrs3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
         client = MongoClient(url)
         db = client["test_db"]
-        collection = db.tokens_database
-        collection = collection.find_one({}, {"_id": 0})
 
-        db = client[db_name]
-        collection = db[collection_name]
-
-        result = db.tokens_database.update_one(collection["whitelist"], data)
+        result = db.whitelist.update_one({'_id': card_id}, {"$set": data}, upsert=True)
