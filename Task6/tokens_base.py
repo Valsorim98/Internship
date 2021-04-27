932fec23bc741e12a80d39d7833b7e1db519e501
Valsorim98 <vt.stanchev@gmail.com>
2021-04-27 13:29:27 +0300

    Check if the token exists in one of the collections and if the user wants to delete it from it and add it to the other collection

--- Task6/tokens_base.py
+++ Task6/tokens_base.py
@@ -37,19 +37,29 @@ class Tokens():
         client = MongoClient(url)
         
         db = client["test_db"]
+        # Find all the documents in the collections to make them iterable
         whitelist = db.whitelist
+        whitelist = whitelist.find()
         blacklist = db.blacklist
+        blacklist = blacklist.find()
+
+        # Update the token id from the database
+        # doc = db.blacklist.find_one({"_id": "6E536046010080FF"})
+        # doc["_id"] = "751E9168661790FF"
+        # db.blacklist.insert(doc)
+        # db.blacklist.remove({"_id": "6E536046010080FF"})
 
         # Iterate through blacklist collection
         for item in blacklist:
             db_card_id = item["_id"]
             if db_card_id == card_id:
                 print("The token already exists in the blacklist collection.")
-                question = input("Do you want me to delete it from blacklist and transfer it to whitelist or not?")
+                question = input("Do you want me to delete it from blacklist and transfer it to whitelist?: ")
                 if question == "yes":
-                    # Delete the document from blacklist here
-
-                    result = db.whitelist.update_one({'_id': card_id}, {"$set": data}, upsert=True)
+                    # Delete the document from blacklist
+                    db.blacklist.delete_one(item)
+                    # Insert the document in whitelist
+                    db.whitelist.update_one({'_id': card_id}, {"$set": data}, upsert=True)
                 if question == "no":
                     break
 
@@ -58,10 +68,11 @@ class Tokens():
             db_card_id = item["_id"]
             if db_card_id == card_id:
                 print("The token already exists in the whitelist collection.")
-                question = input("Do you want me to delete it from whitelist and transfer it to blacklist or not?")
+                question = input("Do you want me to delete it from whitelist and transfer it to blacklist?: ")
                 if question == "yes":
-                    # Delete the document from whitelist here
-
-                    result = db.whitelist.update_one({'_id': card_id}, {"$set": data}, upsert=True)
+                    # Delete the document from whitelist
+                    db.whitelist.delete_one(item)
+                    # Insert the document in blacklist
+                    db.blacklist.update_one({'_id': card_id}, {"$set": data}, upsert=True)
                 if question == "no":
                     break
