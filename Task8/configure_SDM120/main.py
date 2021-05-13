49bb917c818f9bcf8ceac4d9a9ff07f552ac4647
Valsorim98 <vt.stanchev@gmail.com>
2021-05-13 15:55:04 +0300

    Add checks if the input is a string

--- Task8/configure_SDM120/main.py
+++ Task8/configure_SDM120/main.py
@@ -99,7 +99,7 @@ def change_devide_baudrate(current_id, new_baudrate):
 
     # Unpack bytes to binary
     unpack_value = unpack("<HH", byte_value)
-    
+
     # Append the lower number on last position for little-endian
     regs_value = []
     regs_value.append(unpack_value[1])
@@ -112,7 +112,7 @@ def main():
     global client
 
     # Create a connection with the device.
-    client = ModbusClient(method="rtu", port="COM5",
+    client = ModbusClient(method="rtu", port="COM3",
     timeout=1, parity="N", baudrate=9600)
     connection = client.connect()
 
@@ -127,26 +127,40 @@ def main():
     while not is_valid_id:
         # Input from the user for ID - 1 ~ 247.
         input_id = input("Enter a new ID - 1 ~ 247: ")
-        new_id = float(input_id)
 
-        # Only values from 1 to 247 can be passed.
-        if new_id < 1 or new_id > 247:
-            print('Invalid value! Insert 1 ~ 247.')
-        else:
-            is_valid_id = True
+        if input_id.isalpha():
+            print("Invalid value! Type a number 1 ~ 247.")
+
+        if input_id.isdigit():
+            new_id = float(input_id)
+
+            # Only values from 1 to 247 can be passed.
+            if new_id < 1 or new_id > 247:
+                print('Invalid value! Insert 1 ~ 247.')
+
+            else:
+                is_valid_id = True
+
 
     is_valid_baudrate = False
 
     while not is_valid_baudrate:
         # Input from the user for a new baudrate
         input_baudrate = input("For 2400 baud: 0,\nFor 4800 baud: 1,\nFor 9600 baud: 2,\nFor 1200 baud: 5.\nEnter a new baudrate: ")
-        new_device_baudrate = float(input_baudrate)
 
-        # Only values equal to 2400, 4800, 9600 or 1200 can be passed.
-        if new_device_baudrate != 0 and new_device_baudrate != 1 and new_device_baudrate != 2 and new_device_baudrate != 5:
-            print('Invalid value! Insert 0, 1, 2 or 5.')
-        else:
-            is_valid_baudrate = True
+        if input_baudrate.isalpha():
+            print("Invalid value! Type a number 0, 1, 2 or 5.")
+
+        if input_baudrate.isdigit():
+            new_device_baudrate = float(input_baudrate)
+
+            # Only values equal to 2400, 4800, 9600 or 1200 baudrate can be passed.
+            if new_device_baudrate != 0 and new_device_baudrate != 1 and new_device_baudrate != 2 and new_device_baudrate != 5:
+                print('Invalid value! Insert 0, 1, 2 or 5.')
+
+            else:
+                is_valid_baudrate = True
+
 
     time_to_stop = False
 
